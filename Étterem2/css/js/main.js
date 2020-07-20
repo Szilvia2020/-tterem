101161bed33a0c4ec1c9e8716452c9d1d400a467
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2020-07-20 18:08:53 +0200

    Updata main.js

--- Étterem2/css/js/main.js
+++ Étterem2/css/js/main.js
@@ -1,7 +1,25 @@
-ordersForm(){
+function calcAmount(){
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
 
+    showSumPrice(price, amountNumber);
 }
 
-calcAmount(){
-    
+function showSumPrice(price=1000, amountNumber=1){
+    let showAmount = document.querySelector("span.show-amount");
+    if(amountNumber > 10){
+        alert("Maximum 10 terméket vásárolhat!");
+    }else if(amountNumber < 1){
+        alert("Legalább 1 terméket kell vásárolnia!");
+    }else{
+        let amount = amountNumber * price;
+        showAmount.innerHTML = amount;
+    }
+
+    let sendButton = document.querySelector("form.btn.btn-primary");
+    sendButton.onclick = function(){
+        alert("Hello Js");
+    }
 }
