80cbf206fbb34e52aed0143c0f671c5934ce7678
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2020-08-22 19:25:27 +0200

    fix

--- Étterem2/css/js/main.js
+++ Étterem2/css/js/main.js
@@ -1,7 +1,9 @@
 function calcAmount(){
     let price = 1000;
+    //alert(price);
     let amountInput = document.querySelector("input[name='amount-input']");
     let amountNumber = parseInt(amountInput.value);
+    //alert(amountNumber);
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
 
     showSumPrice(price, amountNumber);
@@ -9,15 +11,16 @@ function calcAmount(){
 
 function showSumPrice(price=1000, amountNumber=1){
     amountNumber=amountNumber || 0;
-    let showAmount = document.querySelector("span.showAmount");
+    let showAmount = document.querySelector("span.show-amount");
     if(amountNumber > 10){
         alert("Maximum 10 terméket vásárolhat!");
     }else if(amountNumber < 1){
         alert("Legalább 1 terméket kell vásárolnia!");
     }else{
-
+        let amountInput = document.querySelector("input[name='amount-input']");
+        alert(amountInput.value);
         let amount=parseInt(amountInput.value)* price;
-    
+        alert(amount);
         showAmount.innerHTML = amount;
     }
 }
