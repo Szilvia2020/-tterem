583692a727c1c85a91948af44ffa48389d9476cd
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2020-08-28 18:23:41 +0200

    currency

--- Étterem2/css/js/main.js
+++ Étterem2/css/js/main.js
@@ -1,15 +1,14 @@
+
 function calcAmount(){
     let price = 1000;
-    //alert(price);
-    let amountInput = document.querySelector("input[name='amount-input']");
+    let amountInput = document.querySelector("input[name='amount-input-food']");
     let amountNumber = parseInt(amountInput.value);
-    //alert(amountNumber);
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
 
     showSumPrice(price, amountNumber);
 }
 
-function showSumPrice(price=1000, amountNumber=1){
+function showSumPrice(price=0, amountNumber=1){
     amountNumber=amountNumber || 0;
     let showAmount = document.querySelector("span.show-amount");
     if(amountNumber > 10){
@@ -17,10 +16,28 @@ function showSumPrice(price=1000, amountNumber=1){
     }else if(amountNumber < 1){
         alert("Legalább 1 terméket kell vásárolnia!");
     }else{
-        let amountInput = document.querySelector("input[name='amount-input']");
-        alert(amountInput.value);
+        let amountInput = document.querySelector("input[name='amount-input-food']");
+        let nameOfFood = document.querySelector("input[name='top-input-food']");
+        price = getPriceByName(nameOfFood.value);
         let amount=parseInt(amountInput.value)* price;
-        alert(amount);
         showAmount.innerHTML = amount;
     }
+}
+
+function getPriceByName(name)
+{
+    var price = 0;
+    var f = $.getJSON("http://localhost:3000/food", function(database){
+            $.each(database, function(key, value){
+                if(value.name === name)
+                {
+                    alert(value.name + " " + value.price);
+                    price = parseInt(value.price);
+                    alert(price);
+                }
+            });
+    });
+    $.when(f);
+    alert("lofasz");
+    return price;
 }
