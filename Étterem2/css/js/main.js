2a9042a822cae7013c894dc8d6fc9be72c920f79
jkrizsan <57282556+jkrizsan@users.noreply.github.com>
2020-09-07 19:53:55 +0200

    d

--- Étterem2/css/js/main.js
+++ Étterem2/css/js/main.js
@@ -1,6 +1,8 @@
 
+var price = 0;
+
 function calcAmount(){
-    let price = 1000;
+    //let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input-food']");
     let amountNumber = parseInt(amountInput.value);
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
@@ -18,26 +20,48 @@ function showSumPrice(price=0, amountNumber=1){
     }else{
         let amountInput = document.querySelector("input[name='amount-input-food']");
         let nameOfFood = document.querySelector("input[name='top-input-food']");
-        price = getPriceByName(nameOfFood.value);
+        // price = 
+        getPriceByName(nameOfFood.value);
         let amount=parseInt(amountInput.value)* price;
         showAmount.innerHTML = amount;
     }
 }
 
-function getPriceByName(name)
+function  getPriceByName(name)
 {
-    var price = 0;
-    var f = $.getJSON("http://localhost:3000/food", function(database){
+    // var response = $.getJSON('http://localhost:3000/food');
+    // alert(response);
+    // if (response.ok) { 
+    //     let json =  response.json();
+    //     alert(json);
+    //   } else {
+    //     alert("HTTP-Error: " + response.status);
+    //   }
+
+    //var price = 0;
+    var jqxhr = $.getJSON("http://localhost:3000/food", function(database){
             $.each(database, function(key, value){
                 if(value.name === name)
                 {
-                    alert(value.name + " " + value.price);
                     price = parseInt(value.price);
-                    alert(price);
+                    alert("p1: "+price);
                 }
             });
-    });
-    $.when(f);
-    alert("lofasz");
-    return price;
+    }).done(function() {
+        alert( "second success" );
+      })
+      .fail(function() {
+        alert( "error" );
+      })
+      .always(function() {
+        //alert( "complete" );
+      });
+
+    //    jqxhr.complete(function() {
+    //     alert( "second complete: " + price );
+    //   });
+    alert("p2: "+price);
+   
+    //alert(price);
+   // return price;
 }
